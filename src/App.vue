5028064e1739932a85756771e89323108370aa60
Caroline Liard <caroline.liard@ylly.fr>
2021-09-07 13:58:47 +0200

    :sparkles: feat: Build pause function & add clicks events on buttons to launch music

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,8 @@
           </h2>
           <div class="control">
             <button class="prev">Prev</button>
-            <button class="play" v-if="!isPlaying">Play</button>
-            <button class="pause" v-else >Pause</button>
+            <button class="play" v-if="!isPlaying" @click="play">Play</button>
+            <button class="pause" v-else @click="pause">Pause</button>
             <button class="next">Next</button>
           </div>
       </section>
@@ -54,6 +54,10 @@ export default {
       this.player.play();
       this.isPlaying = true;
       }
+    },
+    pause () {
+      this.player.pause();
+      this.isPlaying = false;
     },
 
   created () {
