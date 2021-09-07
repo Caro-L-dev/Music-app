c6c552bf047a12b4cf58c12a7038fe569e02b811
Caroline Liard <caroline.liard@ylly.fr>
2021-09-07 15:05:39 +0200

    :sparkles: feat: Playlist creation

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,118 @@
 <template>
-  <div id="app">
-    <header>
-      <h1>My Music</h1>
-    </header>
-    <main>
-      <section class="player">
-        <h2 class="song-title">
-          {{ current.title }} -
-          <span>
-            {{ current.artist }}
-            </span>
-          </h2>
-          <div class="control">
-            <button class="prev">Prev</button>
-            <button class="play" v-if="!isPlaying" @click="play">Play</button>
-            <button class="pause" v-else @click="pause">Pause</button>
-            <button class="next">Next</button>
-          </div>
-      </section>
-    </main>
-  </div>
+    <div id="app">
+        <header>
+            <h1>My Music</h1>
+        </header>
+        <main>
+            <section class="player">
+                <h2 class="song-title">
+                    {{ current.title }} -
+                    <span>
+                        {{ current.artist }}
+                    </span>
+                </h2>
+                <div class="control">
+                    <button class="prev" @click="prev">Prev</button>
+                    <button class="play" v-if="!isPlaying" @click="play">Play</button>
+                    <button class="pause" v-else @click="pause">Pause</button>
+                    <button class="next" @click="next">Next</button>
+                </div>
+            </section>
+            <section class="playlist">
+                <h3>The Playlist</h3>
+                <button 
+                  v-for="song in songs" 
+                  :key="song.src" 
+                  @click="play(song)" 
+                  :class="song.src == current.src ? 'song playing' : 'song'"
+                  >
+                  {{ song.title }} ~ {{ song.artist }}
+                </button>
+            </section>
+        </main>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "app",
-  data () {
-    return {
-      current: {},
-      index: 0,
-      songs: [
-        {
-          title: 'Tokyo Revenger OST',
-          artist: '...',
-          src: require('./assets/tokyo-revengers-epic-ost.mp3')
-        },
-        {
-        title: 'Above - Haikyuu',
-        artist: 'Yuki Hayashi',
-        src: require('./assets/above-haikyuu-season-2-ost-vol-1-yuki-hayashi.mp3')
-        }
-      ],
-      player: new Audio()
-    }
-  },
-  methods: {
-    play (song) {
-      if (typeof song.src != "undefined") {
-        this.current = song;
-        this.player.src = this.current.src;
-      }
-  
-      this.player.play();
-      this.isPlaying = true;
-      }
+    name: "app",
+    data() {
+        return {
+            current: {},
+            index: 0,
+            isPlaying: false,
+            songs: [{
+                    title: "Tokyo Revenger OST",
+                    artist: "...",
+                    src: require("./assets/tokyo-revengers-epic-ost.mp3"),
+                },
+                {
+                    title: "Above - Haikyuu",
+                    artist: "Yuki Hayashi",
+                    src: require("./assets/above-haikyuu-season-2-ost-vol-1-yuki-hayashi.mp3"),
+                },
+            ],
+            player: new Audio(),
+        };
     },
-    pause () {
-      this.player.pause();
-      this.isPlaying = false;
+    methods: {
+        play(song) {
+            if (typeof song.src != "undefined") {
+                this.current = song;
+                this.player.src = this.current.src;
+            }
+
+            this.player.play();
+            this.isPlaying = true;
+        },
+    },
+    pause() {
+        this.player.pause();
+        this.isPlaying = false;
+    },
+    next() {
+        this.index++;
+        if (this.index > this.songs.length - 1) {
+            this.index = 0;
+        }
+
+        this.current = this.songs[this.index];
+        this.play(this.current);
+    },
+    prev() {
+        this.index--;
+        if (this.index < 0) {
+            this.index = this.songs.lenght - 1;
+        }
+
+        this.current = this.songs[this.index];
+        this.play(this.current);
     },
 
-  created () {
-    this.current = this.songs[this.index];
-    this.player.src = this.current.src;
-  }
-}
+    created() {
+        this.current = this.songs[this.index];
+        this.player.src = this.current.src;
+    },
+};
 </script>
 
 <style>
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
+
 body {
-  font-family: sans-serif;
+    font-family: sans-serif;
 }
+
 header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 15px;
-  background-color: black;
-  color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 15px;
+    background-color: black;
+    color: white;
 }
 </style>
